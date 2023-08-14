3023008fa0411cdfdd785b613d0aa37c66db90a2
Isa <iruizord@gmail.com>
2023-08-14 02:37:21 -0600

    changing port

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3302
+        port: 3002
     });}
-    
+
 module.exports = sequelize;
