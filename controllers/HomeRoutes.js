e9211d667e2ce6f9465ac2376a35e89696d0f55b
Isa <iruizord@gmail.com>
2023-08-15 01:15:08 -0600

    added routes for the individual posts displaying, the login and the signup

--- controllers/HomeRoutes.js
+++ controllers/HomeRoutes.js
@@ -35,3 +35,48 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const dbPostData = await Post.findOne({
+            where: { id: req.params.id },
+            attributes: ['id', 'content', 'title', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        
+        if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+            res.render('single-post', { post, loggedIn: req.session.loggedIn, username: req.session.username });
+        } else {
+            res.status(404).json({ message: "This id has no post."});
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }   
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+    } else {
+        res.render('login');
+    }
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+module.exports = router;
