c9984c88100bd74bbb78774dd56ffc543b835b3c
Isa <iruizord@gmail.com>
2023-08-15 18:47:14 -0600

    trying to deploy to heroku

--- config/config.js
+++ config/config.js
@@ -2,13 +2,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+
+if (process.env.NODE_ENV === 'production') {
+    // Use JawsDB MySQL database on Heroku
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+        dialect: 'mysql',
+    });
 } else {
-  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    });}
+    // Use local MySQL database for development
+    sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    });
+}
 
 module.exports = sequelize;
