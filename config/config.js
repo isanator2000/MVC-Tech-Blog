2150d15f5ac7ae2c65a391a4274a353d634f9efa
Isa <iruizord@gmail.com>
2023-08-15 18:55:50 -0600

    fixing config file

--- config/config.js
+++ config/config.js
@@ -2,13 +2,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    });}
+    sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT || 3306,
+    });
+}
 
 module.exports = sequelize;
