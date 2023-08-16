d482ab197a13a32ccf469645057d586741e22edd
Isa <iruizord@gmail.com>
2023-08-15 21:30:07 -0600

    fixing code

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,6 @@
 const logoutHandler = async () => {
     try {
-        const response = await fetch('/api/user/logout', {
+        const response = await fetch('/api/User/logout', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
         });
