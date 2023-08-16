61c537785388868b1691beb5c8a0a7cb79743a68
Isa <iruizord@gmail.com>
2023-08-15 18:59:13 -0600

    trying to solve heroku errors

--- config/config.js
+++ config/config.js
@@ -2,14 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT || 3306,
+        port: process.env.DB_PORT,
     });
 }
 
