db3e9658cae3f656ddc5a23f7c98e0f88e5157c3
Isa <iruizord@gmail.com>
2023-08-17 18:29:42 -0600

    added signup to api route

--- public/js/signup.js
+++ public/js/signup.js
@@ -8,7 +8,7 @@ const signupFormHandler = async (event) => {
   const password = passwordInput.value.trim();
 
   try {
-      const response = await fetch('/api/user', {
+      const response = await fetch('/api/user/signup', {
           method: 'POST',
           body: JSON.stringify({
               username,
